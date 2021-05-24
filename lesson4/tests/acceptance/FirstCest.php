bfb0b060d402f847291d13bb7d6f5c34d29b9e66
kabylbekovgabit <kabylbekovgabit@kazdream.kz>
2021-05-24 16:27:02 +0600

    refactor: deleted SecondCert.php and add: func test

--- lesson4/tests/acceptance/FirstCest.php
+++ lesson4/tests/acceptance/FirstCest.php
@@ -7,17 +7,19 @@ class FirstCest
     }
 
     //**test */
-    public function tryToTest(AcceptanceTester $I)
+    public function CheckGoodsOnModalWindow(AcceptanceTester $I)
     {
+        //first page actions
         $I->amOnPage('');
         $I->waitForElementClickable('#search_query_top', 10);
         $I->seeElement('#search_query_top');
         $I->click('#search_query_top');
         $I->fillField('#search_query_top', 'Blouse');
         $I->click('#searchbox > button');
-        $I->waitForElementClickable('#center_column > ul > li > div > div.left-block > div > a.quick-view', 20);
-        $I->seeElement('#center_column > ul > li > div > div.left-block > div > a.quick-view > span');
-        $I->click('#center_column > ul > li > div > div.left-block > div > a.quick-view');
+
+        //second page actions   
+        $I->waitForElementVisible('#center_column > ul > li > div > div.left-block > div', 20);
+        $I->click('#center_column > ul > li > div > div.left-block > div > a.quick-view > span');
         $I->wait(10);
         $I->see('Blouse','#product > div:nth-child(2) > div > div.pb-center-column.col-xs-12.col-sm-4 > h1');
     }
---
+++ lesson4/tests/functional/PostsCheckerCest.php
@@ -0,0 +1,19 @@
+<?php
+
+class PostsCheckerCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    //**test   */
+    public function CheckGoodsOnModalWindow(FunctionalTester $I)
+    {
+        $I->amOnPage('');
+        $I->seeElement('#search_query_top');
+        $I->click('#search_query_top');
+        $I->fillField('#search_query_top', 'Printed dress');
+        $I->click('#searchbox > button');
+        $I->seeNumberOfElements('product_list grid row', 5);
+    }
+}
