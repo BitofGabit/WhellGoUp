85355099f1e24c5e5fca1d812fb5affadd59b917
kabylbekovgabit <kabylbekovgabit@kazdream.kz>
2021-05-24 17:47:11 +0600

    add: new branch

--- lesson4/tests/acceptance/FirstCest.php
+++ lesson4/tests/acceptance/FirstCest.php
@@ -21,7 +21,7 @@ class FirstCest
         $I->wait(10);
         $I->click(['class' => 'quick-view']);
         $I->switchToIFrame('fancybox-frame1621854109526', 
-        'fancybox-frame1621854109526', 
+        'fancybox-frame1621854109526',  
         'http://automationpractice.com/index.php?id_product=2&amp;controller=product&amp;search_query=Blouse&amp;results=1&amp;content_only=1');
         $I->wait(10);
         $I->see('Blouse','#product > div:nth-child(2) > div > div.pb-center-column.col-xs-12.col-sm-4 > h1');
